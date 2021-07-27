b4d5e59e26203b4a1e05b4317b44be8ec0059973
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-07-28 00:14:19 +0300

    Init

---
+++ src/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ propName }}</div>
+  <div>{{ dataName }}</div>
+  <div>{{ setupName }}</div>
+</template>
+
+<script>
+export default {
+  props: { propName: String },
+  // data: () => ({ dataName: 'hello' }),
+  setup: () => ({ setupName: 'here' }),
+};
+</script>
